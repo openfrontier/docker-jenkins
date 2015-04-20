6002b180425901dd931fdacb5ac8edc958def2f1
zsx <thinkernel@gmail.com>
2015-04-20 15:33:07 +0800

    Remove plugin installation from scripts.

--- createJenkins.sh
+++ createJenkins.sh
@@ -4,5 +4,5 @@ JENKINS_NAME='jenkins-master'
 GERRIT_NAME='gerrit'
 LOCAL_VOLUME=~/jenkins_volume
 mkdir -p ${LOCAL_VOLUME}
-docker run --name $JENKINS_NAME --link $GERRIT_NAME:gerrit -p 8088:8080 -v ${LOCAL_VOLUME}:/var/jenkins_home -d jenkins
+docker run --name $JENKINS_NAME --link $GERRIT_NAME:gerrit -p 8088:8080 -v ${LOCAL_VOLUME}:/var/jenkins_home -d openfrontier/jenkins
 
