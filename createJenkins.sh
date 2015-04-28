2abdce217fe530da5e7ed40c39835307d30b9948
zsx <thinkernel@gmail.com>
2015-04-28 14:58:46 +0800

    Support reverse proxy.

--- createJenkins.sh
+++ createJenkins.sh
@@ -4,6 +4,7 @@ JENKINS_NAME=${JENKINS_NAME:-jenkins-master}
 GERRIT_NAME=${GERRIT_NAME:-gerrit}
 JENKINS_IMAGE_NAME=${JENKINS_IMAGE_NAME:-openfrontier/jenkins}
 LOCAL_VOLUME=~/jenkins_volume
+JENKINS_OPTS=${JENKINS_OPTS:---prefix=/jenkins}
 
 mkdir -p "${LOCAL_VOLUME}"
-docker run --name ${JENKINS_NAME} --link ${GERRIT_NAME}:gerrit -p 8088:8080 -v ${LOCAL_VOLUME}:/var/jenkins_home -d ${JENKINS_IMAGE_NAME}
+docker run --name ${JENKINS_NAME} --link ${GERRIT_NAME}:gerrit -p 8088:8080 -v ${LOCAL_VOLUME}:/var/jenkins_home -d ${JENKINS_IMAGE_NAME} ${JENKINS_OPTS}
