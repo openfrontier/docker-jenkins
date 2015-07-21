5bf4f913d24a3c34a029c325a22818c66d4d000b
zsx <thinkernel@gmail.com>
2015-07-21 14:37:54 +0800

    Support jenkins docker plugin.
    Remove maven installation which is included in slave docker image.
    
    Add docker cloud configuration.

--- Dockerfile
+++ Dockerfile
@@ -13,14 +13,12 @@ COPY credentials.xml /usr/local/etc/credentials.xml
 
 # Add maven installation config file
 COPY hudson.tasks.Maven.xml /usr/local/etc/hudson.tasks.Maven.xml
-COPY apache-maven-3.2.2-bin.zip /usr/local/etc/apache-maven-3.2.2-bin.zip
-COPY maven.installedFrom /usr/local/etc/.installedFrom
 
 # Add Jenkins URL and system admin e-mail config file
 COPY jenkins.model.JenkinsLocationConfiguration.xml /usr/local/etc/jenkins.model.JenkinsLocationConfiguration.xml
 
-# Add Nexus repository mirror config file
-COPY maven.settings.xml /usr/local/etc/settings.xml
-
 # Add setup script.
 COPY jenkins-setup.sh /usr/local/bin/jenkins-setup.sh
+
+# Add cloud setting in config file.
+COPY config.xml /usr/local/etc/config.xml
--- jenkins-setup.sh
+++ jenkins-setup.sh
@@ -22,12 +22,6 @@ cp /usr/local/etc/credentials.xml ${JENKINS_HOME}/credentials.xml
 
 # Setup maven installation
 cp /usr/local/etc/hudson.tasks.Maven.xml ${JENKINS_HOME}/hudson.tasks.Maven.xml
-mkdir -p /var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation
-unzip -q /usr/local/etc/apache-maven-3.2.2-bin.zip -d /var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation
-mv /var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/apache-maven-3.2.2 \
-   /var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/Maven-3.2.2
-cp /usr/local/etc/.installedFrom \
-   /var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/Maven-3.2.2/.installedFrom
 
 # Replace '/' in url to '\/'
 [ "${JENKINS_WEBURL%/}" = "${JENKINS_WEBURL}" ] && JENKINS_WEBURL="${JENKINS_WEBURL}/"
@@ -42,18 +36,5 @@ cp /usr/local/etc/jenkins.model.JenkinsLocationConfiguration.xml \
 sed -i "s/{JENKINS_URL}/${JENKINS_URL}/g" \
   ${JENKINS_HOME}/jenkins.model.JenkinsLocationConfiguration.xml
 
-# Setup nexus as maven mirror if defined
-if [ ${#NEXUS_REPO} -gt 0 ]; then
-  # Replace '/' in url to '\/'
-  [ "${NEXUS_REPO%/}" = "${NEXUS_REPO}" ] && NEXUS_REPO="${NEXUS_REPO}/"
-  while [ -n "${NEXUS_REPO}" ]; do
-  NEXUS_URL="${NEXUS_URL}${NEXUS_REPO%%/*}\/"
-  NEXUS_REPO="${NEXUS_REPO#*/}"
-  done
-  NEXUS_URL="${NEXUS_URL%\\/}"
-
-  # Setup maven repository mirror
-  mkdir -p ${JENKINS_HOME}/.m2
-  cp /usr/local/etc/settings.xml ${JENKINS_HOME}/.m2/settings.xml
-  sed -i "s/{{NEXUS_URL}}/${NEXUS_URL}/g" ${JENKINS_HOME}/.m2/settings.xml
-fi
+# Setup Jenkins Docker
+cp /usr/local/etc/config.xml ${JENKINS_HOME}/config.xml
