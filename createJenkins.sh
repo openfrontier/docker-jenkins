9495e33c6d3f5c10b77ebdd88a1cb40c2519e74b
thinkernel <thinkernel@gmail.com>
2016-03-28 19:52:58 +0800

    Remove mounting of /var/run/docker.sock
    tcp is used instead.

--- createJenkins.sh
+++ createJenkins.sh
@@ -21,7 +21,6 @@ docker run \
 --name ${JENKINS_NAME} \
 --link ${GERRIT_NAME}:gerrit \
 -p 50000:50000 \
--v /var/run/docker.sock:/var/run/docker.sock \
 -v /bin/docker:/bin/docker \
 --volumes-from ${JENKINS_VOLUME} \
 -e JAVA_OPTS="-Duser.timezone=${TIMEZONE}" \
