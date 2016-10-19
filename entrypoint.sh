8c807c03f87756f2bac06484fba58b9c06f97ff5
mengzhaopeng <qiuranke@gmail.com>
2016-10-19 19:05:02 +0800

    var_cmd_start add ""

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "Genarate JENKINS SSH KEY"
 source /usr/local/bin/generate_key.sh
 echo "start JENKINS"
 var_cmd_start=`echo $@|cut -c 1-2`
-if [ ${var_cmd_start} = "--" ]; then
+if [ "${var_cmd_start}" == "--" ]; then
     exec /bin/tini -- /usr/local/bin/jenkins.sh $@
 fi
 exec "$@"
