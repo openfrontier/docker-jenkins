7b886e4de7286613fc117d59ded024ce64c7fc56
mengzhaopeng <qiuranke@gmail.com>
2016-10-19 18:39:31 +0800

    entrypoint cmd to deal with

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 echo "Genarate JENKINS SSH KEY"
 source /usr/local/bin/generate_key.sh
 echo "start JENKINS"
-if [ "x$1" == "x" ]; then
-    exec /bin/tini -- /usr/local/bin/jenkins.sh --prefix=/jenkins
+var_cmd_start=`echo $@|cut -c 1-2`
+if [ ${var_cmd_start} = "--" ]; then
+    exec /bin/tini -- /usr/local/bin/jenkins.sh $@
 fi
 exec "$@"
