d735b795e36f6e2e8dfef682cb55df393331c8e1
mengzhaopeng <qiuranke@gmail.com>
2016-10-21 17:24:54 +0800

    if conditions change to docker jenkins format and generate_key.sh delete cd operation.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 echo "Genarate JENKINS SSH KEY"
 source /usr/local/bin/generate_key.sh
 echo "start JENKINS"
-if [[ "$@" =~ ^-- ]]; then
+# if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
+if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
     exec /bin/tini -- /usr/local/bin/jenkins.sh "$@"
 fi
 exec "$@"
--- generate_key.sh
+++ generate_key.sh
@@ -8,11 +8,10 @@ JENKINS_USER_CONTENT_DIR="${JENKINS_HOME}/userContent/"
 
 echo "Generating Jenkins Key Pair"
 if [ ! -d "${JENKINS_SSH_DIR}" ]; then mkdir -p "${JENKINS_SSH_DIR}"; fi
-cd "${JENKINS_SSH_DIR}"
 
-if [ ! -f "id_rsa" ]; then
-    rm -f id_rsa.pub
-    ssh-keygen -t rsa -f 'id_rsa' -b 4096 -N '';
+if [ ! -f ${JENKINS_SSH_DIR}/"id_rsa" ]; then
+    rm -f ${JENKINS_SSH_DIR}/id_rsa.pub
+    ssh-keygen -t rsa -f ${JENKINS_SSH_DIR}/'id_rsa' -b 4096 -N '';
     echo "Copy key to userContent folder"
     mkdir -p ${JENKINS_USER_CONTENT_DIR}
     rm -f ${JENKINS_USER_CONTENT_DIR}/id_rsa.pub
