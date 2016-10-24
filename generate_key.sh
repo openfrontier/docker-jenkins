ccece9ae5a15b81e449981008993311f3304a1d6
thinkernel <thinkernel@gmail.com>
2016-10-24 11:22:13 +0000

    Change echo position of key generation

--- generate_key.sh
+++ generate_key.sh
@@ -6,10 +6,10 @@ JENKINS_HOME="/var/jenkins_home"
 JENKINS_SSH_DIR="${JENKINS_HOME}/.ssh"
 JENKINS_USER_CONTENT_DIR="${JENKINS_HOME}/userContent/"
 
-echo "Generating Jenkins Key Pair"
 if [ ! -d "${JENKINS_SSH_DIR}" ]; then mkdir -p "${JENKINS_SSH_DIR}"; fi
 
 if [ ! -f ${JENKINS_SSH_DIR}/"id_rsa" ]; then
+    echo "Generating Jenkins Key Pair"
     rm -f ${JENKINS_SSH_DIR}/id_rsa.pub
     ssh-keygen -t rsa -f ${JENKINS_SSH_DIR}/'id_rsa' -b 4096 -N '';
     echo "Copy key to userContent folder"
