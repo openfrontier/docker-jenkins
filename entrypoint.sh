2635a79da7c49927455e1f1ee0a8e3e0bcab0f17
thinkernel <thinkernel@gmail.com>
2018-02-06 09:56:57 +0000

    Remove full path when exec tini

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ source /usr/local/bin/generate_key.sh
 echo "start JENKINS"
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
-    exec /bin/tini -- /usr/local/bin/jenkins.sh "$@"
+    exec tini -- /usr/local/bin/jenkins.sh "$@"
 fi
 exec "$@"
